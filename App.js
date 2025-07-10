f5b7ae080312c61aa450063af31fc2eec5a6c409
csaavedra <gabyc_gsm@yahoo.es>
2025-07-10 18:07:39 -0500

    componentes react

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { getPersonajes } from './lib/rickyMorty';
 import { ScrollView } from 'react-native-web';
 import  Logo  from './components/Logo';
+import { CharacterCard } from './components/CharacterCard';
 
 export default function App() {
 
@@ -19,21 +20,18 @@ export default function App() {
   }, []);
 
   return (
-    <ScrollView>
+    
       <View style={styles.container}>
         <StatusBar style='light' />
         <Logo style={styles.logo} />
+        <ScrollView>
         {personajes.map((character) => (
-          <View style={styles.card} key={character.id}>
-            <Image style={styles.image} source={{ uri: character.image }}></Image>
-            <Text style={styles.title}>{character.name}</Text>
-            <Text style={styles.species}>{character.species}</Text>
-            <Text style={styles.status}>{character.status}</Text>
-            <Text style={styles.gender}>{character.gender}</Text>
-          </View>
+          
+          <CharacterCard character={character}/>
         ))}
+        </ScrollView>
       </View>
-    </ScrollView>
+    
   );
 }
 
@@ -44,38 +42,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
-    width: 107,
-    height: 147,
-    borderRadius: 10,
-  },
-  card: {
-    backgroundColor: '#333',
-    padding: 20,
-    borderRadius: 10,
-    marginBottom: 20,
-    width: '90%',
-    alignItems: 'center'
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: '#fff'
-  },
-  species: {
-    fontSize: 20,
-    color: '#fff'
-  },
-  status: {
-    fontSize: 16,
-    color: '#33caff'
-  },
-  gender: {
-    fontSize: 16,
-    color: '#fff',
-    fontWeight: 'bold'
-  },
+  
   logo: {
     backgroundColor: '#fff',
     padding: 20
